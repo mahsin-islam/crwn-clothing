385f97dc0552f8d37505d5a9f4b0d551bf18bd0f
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2022-02-27 15:17:10 +0600

    broken our hompage in smaller componenets, styling our menu item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
