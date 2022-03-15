27bf294398702d09e5d8a09d521ef3455b0601d4
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2022-03-15 16:17:12 +0600

    updating our app with shop page, collection of preview component and collection item component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import './App.css';
+import Home from "./components/Home/Home";
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from "./pages/shop/shop.component";
 
+const HatsPage = () => (
+  <div>
+    <Link to='/topics'>Topics</Link>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+const TopicsList = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topics List PAGE</h1>
+    </div>
+  );
+};
+const TopicDetail = (props) => {
+  console.log(props);
+  return (<div>
+    <h1>Topic Detail PAGE: {props.match.params.topicId}</h1>
+  </div>
+  );
+};
 function App() {
   return (
-    <div className="">
-      <HomePage/>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} ></Route>
+        <Route exact={true} path="/topics" element={<TopicsList />} ></Route>
+        <Route path="/topics/:topicId" element={<TopicDetail />} ></Route>
+        <Route path="/shop" element={<ShopPage />} ></Route>
+
+      </Routes>
+
+      {/* <HomePage></HomePage> */}
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,11 +7,10 @@ const MenuItem = ({ title, imageUrl, size }) => {
                 style={{
                     backgroundImage: `url(${imageUrl})`
                 }} />
-                <div className="content">
-                    <h1 className="title">{title.toUpperCase()}</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
         </div>
     );
 };
